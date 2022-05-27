c444d84c759e9b6fde0559b007213b5f7a943c7b
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-05-27 12:16:50 +0200

    Delete note feature

--- src/App.js
+++ src/App.js
@@ -40,6 +40,13 @@ export default function App() {
     })
   }
 
+  function deleteNote(event, noteId) {
+    event.stopPropagation()
+    setNotes(oldNotes => {
+      return oldNotes.filter(note => note.id !== noteId)
+    })
+  }
+
   function findCurrentNote() {
     return notes.find(note => {
       return note.id === currentNoteId
@@ -61,6 +68,7 @@ export default function App() {
               currentNote={findCurrentNote()}
               setCurrentNoteId={setCurrentNoteId}
               newNote={createNewNote}
+              deleteNote={deleteNote}
             />
             {
               currentNoteId &&
